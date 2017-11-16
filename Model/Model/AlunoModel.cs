2b127a8e04f0715cbbff2280b4ae58aeacff717f
Benyton <Aluno@045LAB02007>
2017-11-16 20:47:39 -0200

    Inclusão da Model e dataAnnotations do Aluno!

--- Model/Model/AlunoModel.cs
+++ Model/Model/AlunoModel.cs
@@ -1,15 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Model
 {
+    [Table("ALUNO")]
     public class AlunoModel
     {
         [Key]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatorio")]
+        [MinLength(4, ErrorMessage = "Deve possuir no mínimo 4 caracteres!")]
+        [MaxLength(15, ErrorMessage = "Deve possuir no mínimo 15 caracteres!")]
+        [Display(Name = "Nome*")]
+        public string nome { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatorio")]
+        [MinLength(3, ErrorMessage = "Deve possuir no mínimo 3 caracteres!")]
+        [MaxLength(3, ErrorMessage = "Deve possuir no mínimo 3 caracteres!")]
+        [Display(Name = "Idade*")]
+        public int idade { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatorio")]
+        [MinLength(4, ErrorMessage = "Deve possuir no mínimo 4 caracteres!")]
+        [MaxLength(15, ErrorMessage = "Deve possuir no mínimo 15 caracteres!")]
+        [Display(Name = "Peso*")]
+        public float peso { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatorio")]
+        [MinLength(3, ErrorMessage = "Deve possuir no mínimo 3 caracteres!")]
+        [MaxLength(3, ErrorMessage = "Deve possuir no mínimo 3 caracteres!")]
+        [Display(Name = "Altura*")]
+        public float altura { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatorio")]
+        [MinLength(5, ErrorMessage = "Deve possuir no mínimo 5 caracteres!")]
+        [MaxLength(15, ErrorMessage = "Deve possuir no mínimo 15 caracteres!")]
+        [Display(Name = "Objetivo*")]
+        public string objetivo { get; set; }
+
+        [ForeignKey("USUARIOID")]
+        public int UsuarioFK { get; set; }
+
+        [ForeignKey("PROFESSORID")]
+        public int ProfessorFK { get; set; }
+
+        public virtual TreinoModel Treino { get; set; }
     }
 }
