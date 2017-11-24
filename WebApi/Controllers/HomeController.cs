255b7cd30240f48b6dec613c387c4a9b1fe189f7
Dayane Paola <dayanepaola@hotmail.com>
2017-11-23 22:45:30 -0200

    Adição da autenticação da tela de login

--- WebApi/Controllers/HomeController.cs
+++ WebApi/Controllers/HomeController.cs
@@ -6,11 +6,17 @@ using System.Web.Mvc;
 
 namespace WebApi.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         // GET: Home
         public ActionResult Index()
         {
+            var auth = System.Web.HttpContext.Current.User.Identity.Name;
+
+            var teste = System.Web.HttpContext.Current.User.IsInRole("Aluno");
+            var teste2 = System.Web.HttpContext.Current.User.IsInRole("Professor");
+
             return View();
         }
     }
--- WebApi/Global.asax.cs
+++ WebApi/Global.asax.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.Security;
 
 namespace WebApi
 {
@@ -13,6 +15,17 @@ namespace WebApi
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        protected void Application_AuthenticateRequest(Object sender, EventArgs e)
+        {
+            HttpCookie authCookie = Context.Request.Cookies[FormsAuthentication.FormsCookieName];
+            if (authCookie != null)
+            {
+                FormsAuthenticationTicket authTicket = FormsAuthentication.Decrypt(authCookie.Value);
+                GenericPrincipal userPrincipal = new GenericPrincipal(new GenericIdentity(authTicket.Name), new string[] { authTicket.UserData });
+                Context.User = userPrincipal;
+            }
         }
     }
 }
